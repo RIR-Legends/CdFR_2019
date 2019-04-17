18f2cb1773e64f5f0e69534ea37854121c03e3b2
Yannou <45776332+YannLocatelli@users.noreply.github.com>
2019-04-17 23:42:31 +0200

    Final Modif
    Dernières modification après des tests sur robot.

--- Deplacements/main.py
+++ Deplacements/main.py
@@ -6,22 +6,29 @@ import param as p
 
 def Demo_1() :
 
-    move = p.Move()
+    move = p.Move
 
     time.sleep(0.1)
-    move.RunToPos(500)
+    move.turnRel(90)
     time.sleep(1)
-    move.TurnRel(-180)
+    move.turnRel(-180)
     time.sleep(1)
-    move.RunToPos(200)
+    move.turnRel(450)
     time.sleep(1)
 
-    move.TurnAbs(0)  # position initiale définie à la mise sous tension des batteries
+    move.turnAbs(0) # position initiale définie à la mise sous tension des batteries
 
     time.sleep(3)
 
 
-
 move = p.Move()
 
-move.Calib()
+move.calib()
+
+Demo_1()
+
+fin()
+
+print('Fin du programme')
+
+#odrv0.reboot()
