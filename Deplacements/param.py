37e13dd2db2c3823af81695c0aae93b769230c86
Thibault ROBERT <tbt.rbt@gmail.com>
2019-04-17 23:06:44 +0200

    Update param.py

--- Deplacements/param.py
+++ Deplacements/param.py
@@ -25,10 +25,11 @@ class Move:
 
         # coding features
         self.ErrorMax = 5      # unité ?
+        self.odrv0 = ODrive.find_any()
 
     def Stop(self):
-        odrv0.axis0.controller.speed(0)
-        odrv0.axis1.controller.speed(0)
+        self.odrv0.axis0.controller.speed(0)
+        self.odrv0.axis1.controller.speed(0)
 
     def WaitEndMove(self, axis, goal, errorMax):
 
@@ -36,7 +37,7 @@ class Move:
         index = 0
         movAvg = abs(goal - axis.encoder.pos_estimate)
         while movAvg >= errorMax:   # A commenter !!
-            print(odrv0.axis1.encoder.pos_estimate)
+            print(self.odrv0.axis1.encoder.pos_estimate)
             for i in range(index, 10):
                 index = 0
                 avg[i] = abs(goal - axis.encoder.pos_estimate)
@@ -54,10 +55,10 @@ class Move:
         target = (self.nbCounts * distance)/self.WheelPerimeter
 
         print(target)
-        odrv0.axis0.controller.move_to_pos(-target)
+        self.odrv0.axis0.controller.move_to_pos(-target)
         # Voir si utilisation necessaire des threads
-        odrv0.axis1.controller.move_to_pos(target)
-        self.WaitEndMove(odrv0.axis1, target, self.ErrorMax)
+        self.odrv0.axis1.controller.move_to_pos(target)
+        self.WaitEndMove(self.odrv0.axis1, target, self.ErrorMax)
 
     def TurnAbs(self, Angle):
 
@@ -73,66 +74,66 @@ class Move:
         target = (self.nbCounts * runAngle) / self.WheelPerimeter
 
         # Action ! :
-        print(odrv0.axis0.encoder.pos_estimate)
-        odrv0.axis0.controller.move_to_pos(target)
-        print(odrv0.axis1.encoder.pos_estimate)
-        odrv0.axis1.controller.move_to_pos(target)
+        print(self.odrv0.axis0.encoder.pos_estimate)
+        self.odrv0.axis0.controller.move_to_pos(target)
+        print(self.odrv0.axis1.encoder.pos_estimate)
+        self.odrv0.axis1.controller.move_to_pos(target)
 
         # Attente de la fin du mouvement
-        self.WaitEndMove(odrv0.axis0, target, errorMax)
-        self.WaitEndMove(odrv0.axis1, target, errorMax)
+        self.WaitEndMove(self.odrv0.axis0, target, errorMax)
+        self.WaitEndMove(self.odrv0.axis1, target, errorMax)
 
     def Calib(self):
 
         # Find a connected ODrive (this will block until you connect one)
         print("finding an odrive...")
-        odrv0 = odrive.find_any()
+        self.odrv0
         print('Odrive found ! ')
 
         # 40Amp max dans le moteur (gros couple et sécurité pour pas fumer le moteur)
-        odrv0.axis0.motor.config.current_lim = 40
-        odrv0.axis1.motor.config.current_lim = 40
+        self.odrv0.axis0.motor.config.current_lim = 40
+        self.odrv0.axis1.motor.config.current_lim = 40
 
         # vmax en tick/s les encodeurs font 8192 tick/tours
         # controller.*.vel_limite prend le pas sur trap_traj.*.vel_limt
-        odrv0.axis0.controller.config.vel_limit = 750000
-        odrv0.axis1.controller.config.vel_limit = 750000
+        self.odrv0.axis0.controller.config.vel_limit = 750000
+        self.odrv0.axis1.controller.config.vel_limit = 750000
 
         # trap_traj parametrage des valeurs limit du comportement dynamique
-        odrv0.axis1.trap_traj.config.vel_limit = 100000
-        odrv0.axis0.trap_traj.config.vel_limit = 100000
+        self.odrv0.axis1.trap_traj.config.vel_limit = 100000
+        self.odrv0.axis0.trap_traj.config.vel_limit = 100000
 
-        odrv0.axis0.trap_traj.config.accel_limit = 30000
-        odrv0.axis1.trap_traj.config.accel_limit = 30000
+        self.odrv0.axis0.trap_traj.config.accel_limit = 30000
+        self.odrv0.axis1.trap_traj.config.accel_limit = 30000
 
-        odrv0.axis0.trap_traj.config.decel_limit = 30000
-        odrv0.axis1.trap_traj.config.decel_limit = 30000
+        self.odrv0.axis0.trap_traj.config.decel_limit = 30000
+        self.odrv0.axis1.trap_traj.config.decel_limit = 30000
 
         '''  # [EN DEV] Fonction pour lancer la calibration si elle n'a pas déjà été lancée '''
-        # if odrv0.axis1.motor.is_calibrated == False:
-        if odrv0.axis1.current_state == 1:  # AXIS_STATE_IDLE
+        # if self.odrv0.axis1.motor.is_calibrated == False:
+        if self.odrv0.axis1.current_state == 1:  # AXIS_STATE_IDLE
 
-            print("etat courant" + str(odrv0.axis1.current_state))
+            print("etat courant" + str(self.odrv0.axis1.current_state))
 
             print("starting calibration...")
 
-            odrv0.axis1.requested_state = 3  # AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+            self.odrv0.axis1.requested_state = 3  # AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 
-            while odrv0.axis1.current_state != 1:  # AXIS_STATE_IDLE
+            while self.odrv0.axis1.current_state != 1:  # AXIS_STATE_IDLE
                 time.sleep(1)
 
-            odrv0.axis1.encoder.pos_estimate()
+            self.odrv0.axis1.encoder.pos_estimate()
 
-            odrv0.axis1.requested_state = 8  # AXIS_STATE_CLOSED_LOOP_CONTROL  , mise en mode boucle fermée
+            self.odrv0.axis1.requested_state = 8  # AXIS_STATE_CLOSED_LOOP_CONTROL  , mise en mode boucle fermée
 
         else:
-            odrv0.axis1.encoder.pos_estimate()
+            self.odrv0.axis1.encoder.pos_estimate()
             print("after else")
 
 
 def Fin():
 
-    odrv0.axis1.requested_state = 1  # AXIS_STATE_IDLE , libère le moteur : boucle ouverte
+    self.odrv0.axis1.requested_state = 1  # AXIS_STATE_IDLE , libère le moteur : boucle ouverte
 
 def TurnRel(Angle):
     # definition des constantes liées au robot
@@ -145,14 +146,14 @@ def TurnRel(Angle):
     # calcul du nombre de ticks a parcourir pour tourner sur place de l'angle demandé
     runAngle = Angle * pi * axlTrack
     target = (nbrCounts * runAngle) / weelPerim
-    targRel = target + (odrv0.axis0.encoder.pos_estimate + odrv0.axis0.encoder.pos_estimate)/2
+    targRel = target + (self.odrv0.axis0.encoder.pos_estimate + self.odrv0.axis0.encoder.pos_estimate)/2
 
     # Action ! :
-    print(odrv0.axis0.encoder.pos_estimate)
-    odrv0.axis0.controller.move_to_pos(targRel)
-    print(odrv0.axis1.encoder.pos_estimate)
-    odrv0.axis1.controller.move_to_pos(-targRel)
+    print(self.odrv0.axis0.encoder.pos_estimate)
+    self.odrv0.axis0.controller.move_to_pos(targRel)
+    print(self.odrv0.axis1.encoder.pos_estimate)
+    self.odrv0.axis1.controller.move_to_pos(-targRel)
 
     # Attente de la fin du mouvement
-    Move.WaitEndMove(odrv0.axis0, targRel, Move.error_max)
-    Move.WaitEndMove(odrv0.axis1, targRel, Move.error_max)
+    Move.WaitEndMove(self.odrv0.axis0, targRel, Move.error_max)
+    Move.WaitEndMove(self.odrv0.axis1, targRel, Move.error_max)
