605d57912c361e1253627f4e58088bd4a009aecc
Thibault ROBERT <tbt.rbt@gmail.com>
2019-04-23 21:43:10 +0200

    Update main.py

--- Deplacements/main.py
+++ Deplacements/main.py
@@ -9,16 +9,16 @@ def demo_1() :
     move = p.Move
 
     time.sleep(0.1)
-    move.turn_rel(90)
+    move.turn_abs(90)
     time.sleep(1)
-    move.turn_rel(-180)
+    move.run_to_pos(50)
+    move.turn_abs(-180)
     time.sleep(1)
-    move.turn_rel(450)
+    move.turn_abs(450)
     time.sleep(1)
-
     move.turn_abs(0) # position initiale définie à la mise sous tension des batteries
-
     time.sleep(3)
+    move.run_to_pos(50)
 
 
 move = p.Move()
