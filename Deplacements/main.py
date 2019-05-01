d08d896ebc06484a85da5c6a9a32835d154bdee9
Thibault ROBERT <tbt.rbt@gmail.com>
2019-05-01 14:56:52 +0200

    Update main.py

--- Deplacements/main.py
+++ Deplacements/main.py
@@ -37,11 +37,11 @@ def demo_tour(odrv0) :
         time.sleep(2)
         move.rotation(-90)
         time.sleep(2)
-        
+
 
 #param.RAZ() # Lance fonction remise à zero des moteurs
 param.config()  #Lance la configuration du odrive (vitesse max / acc max / decc max / courrant max ...)
-
+param.calib_always()
 # Choix de lancement des demos :
 #demo_simple(param.odrv0)
 #demo_tour(param.odrv0)
