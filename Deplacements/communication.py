1eaf9f5bbb04aa1ea39431ce0762836f4e4f2373
Thibault ROBERT <tbt.rbt@gmail.com>
2019-05-04 10:38:45 +0200

    Update communication.py

--- Deplacements/communication.py
+++ Deplacements/communication.py
@@ -18,7 +18,7 @@ class Communication():
     def __init__(self, port = '/dev/ttyASC0'):
         self.__arduino = serial.Serial(port, 9600)
         self.__ard_msg = ""
-        self.__rasp_msg = MSG["Attente"]
+        self.__rasp_msg = Communication.MSG["Attente"]
 
         self.readyNext = True
         self.Tirette = True
@@ -31,32 +31,32 @@ class Communication():
     def send(self,msg):
         self.__rasp_msg = msg
         self.readyNext = False
-        if msg == MSG["Action_Finished"]:
+        if msg == Communication.MSG["Action_Finished"]:
             self.Avancer = False
             self.Reculer = False
 
-        while self.__ard_msg != MSG["Recu"]:
+        while self.__ard_msg != Communication.MSG["Recu"]:
             self.__arduino.write(self.__rasp_msg)
             self.__ard_msg = self.__arduino.readline()
-        self.__rasp_msg = MSG["Attente"]
+        self.__rasp_msg = Communication.MSG["Attente"]
         for i in range(1000):
             self.__arduino.write(self.__rasp_msg)
 
     def read(self, print_rep = False):
-        while self.__ard_msg == MSG["Attente"] or self.__ard_msg == MSG["Recu"]:
+        while self.__ard_msg == Communication.MSG["Attente"] or self.__ard_msg == Communication.MSG["Recu"]:
             self.__ard_msg = self.__arduino.readline()
         self.__interpreter(self.__ard_msg)
 
-        self.__rasp_msg = MSG["Recu"]
-        while self.__arduino.readline() != MSG["Attente"] or self.__arduino.readline() != MSG["Recu"]:
+        self.__rasp_msg = Communication.MSG["Recu"]
+        while self.__arduino.readline() != Communication.MSG["Attente"] or self.__arduino.readline() != Communication.MSG["Recu"]:
             self.__arduino.write(self.__rasp_msg)
 
         if print_rep:
-            print(MSG[self.__ard_msg])
+            print(Communication.MSG[self.__ard_msg])
 
     def check(self):
-        self.__arduino.write(MSG["Attente"])
-        return self.__arduino.readline() != MSG["Recu"] and self.__arduino.readline() != MSG["Attente"]
+        self.__arduino.write(Communication.MSG["Attente"])
+        return self.__arduino.readline() != Communication.MSG["Recu"] and self.__arduino.readline() != Communication.MSG["Attente"]
 
     def checkAndRead(self, print_rep = False):
         if self.check():
@@ -65,20 +65,20 @@ class Communication():
         return False
 
     def __interpreter(self,msg):
-        if msg == MSG["Tirette"]:
+        if msg == Communication.MSG["Tirette"]:
             self.Tirette = False
 
-        if msg == MSG["Action_Finished"]:
+        if msg == Communication.MSG["Action_Finished"]:
             self.readyNext = True
 
-        if msg == MSG["Orange"]:
+        if msg == Communication.MSG["Orange"]:
             self.OrangeSide = True
-        elif msg == MSG["Violet"]:
+        elif msg == Communication.MSG["Violet"]:
             self.OrangeSide = False
 
-        if msg == MSG["Avancer"]:
+        if msg == Communication.MSG["Avancer"]:
             self.Avancer = True
-        elif msg == MSG["Reculer"]:
+        elif msg == Communication.MSG["Reculer"]:
             self.Reculer = True
 
 def test():
