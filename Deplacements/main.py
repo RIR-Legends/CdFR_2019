83357fcb41102caf0b27a02d71ff19a72c61760a
Thibault ROBERT <tbt.rbt@gmail.com>
2019-05-04 15:22:36 +0200

    Update main.py

--- Deplacements/main.py
+++ Deplacements/main.py
@@ -7,10 +7,8 @@ import move as m
 from communication import Communication
 param = p.Param()
 com = Communication()
-Obstacle =
 
-while Obstacle == 1:
-    pass
+
 def demo_simple(odrv0) :
 
     move = m.Move(odrv0)
