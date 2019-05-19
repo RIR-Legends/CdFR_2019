8d6df1b61c9f01015c61500f6550d28da839bd2e
Thibault ROBERT <tbt.rbt@gmail.com>
2019-05-19 19:16:10 +0200

    mis à jour dossier Comm et Arduino

--- Arduino/Test_Com/RIR_communication.h
+++ Arduino/Test_Com/RIR_communication.h
@@ -9,28 +9,41 @@ class RIR_Com{
     ~RIR_Com();
     void RIR_send(int);
     bool RIR_read();
-    
-    int Recu;
-    int Attente;
-    int Action_Finished;
-    
-    int Arret;
-    int Initialisation;
-    int Transport;
-    int Palet_Floor_In;
-    int Palet_Wall_In;
-    int Palet_Floor_Out;
-    int Palet_Wall_Out; 
-    
-    int Tirette;
-    int Violet;
-    int Orange;
-    int Avance;
-    int Recule;
-    
+    void RIR_waitEndMove(int);
+
+    int Reponse;
+
+    enum{
+      // Message commun (public)
+      Action_Finished   = 't',
+
+      // Message entrants
+      Arret             = 'A',
+      Initialisation    = 'I',
+      Transport         = 'T',
+      Palet_Floor_In    = 'f',
+      Palet_Wall_In     = 'w',
+      Palet_Floor_Out   = 'F',
+      Palet_Wall_Out    = 'W', 
+
+      //Message sortants
+      Tirette           = 'D',
+      Violet            = 'v',
+      Orange            = 'o',
+                          
+      Avance            = 'a',
+      Recule            = 'r',
+    };
+        
     private:
     int __ard_msg;
     int __rasp_msg;
+
+    enum {
+      // Message commun (private)
+      __Recu            = '1',
+      __Attente         = '0',
+    };
 };
 
-#endif RIR_COM
+#endif
