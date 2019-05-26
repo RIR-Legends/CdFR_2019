1c6506658ce08751173a7f3ea2184858bbd38ec3
Yannou <45776332+YannLocatelli@users.noreply.github.com>
2019-05-26 02:49:53 +0200

    EKF Class
    Créer la class avec EKF() avec les paramètres appropriés
    
    [nom].estimate([Point]Mouvement, [List(Point)]Points de repères) pour obtenir la position estimée du robot en Point.

---
+++ RPlidar/point.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+class Point:
+    def __init__(self, x, y, theta):
+        self.x = x
+        self.y = y
+        self.theta = theta
+        self.hypo = 0
+
+        self.P = {'X': self.x, 'Y': self.y, 'theta': self.theta}
+
+    def print_pos(self):
+        print('Coordonnée en x = %f ' % self.x)
+        print('Coordonnée en y = %f ' % self.y)
+        print('Theta = %f ' % self.theta)
+
+    def set_parcour(self, ):
+        self.hypo = (self.x ** 2 + self.y ** 2) ** pow(0.5)
+
+    def get_point(self):
+        return self.P
+
+
+def main():
+    point = Point()
+
+    point.print_pos()
+
+
+
+if __name__ == '__main__':
+    main()
