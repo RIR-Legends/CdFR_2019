c4dadb9bd54c0e8d9a3133652cf054ae3613ca68
MartialMAILLOUX <48605370+MartialMAILLOUX@users.noreply.github.com>
2019-05-28 01:48:25 +0200

    sup la tirette

--- Arduino/Test_Com/Test_Com.ino
+++ Arduino/Test_Com/Test_Com.ino
@@ -10,7 +10,7 @@ Setup SetupRobot;
 Elevator ElevatorRobot;
 Pompe PompeRobot;
 Arm ArmRobot; 
-Tirette TiretteRobot; 
+//Tirette TiretteRobot; 
 CapteurPression CapteurPressionRobot;
 
 RIR_Com com;
