7e76be7372bdd13e92b67445b05308d1577cd8ad
Thibault ROBERT <tbt.rbt@gmail.com>
2019-05-28 10:18:36 +0200

    Update move.py

--- Deplacement/Movement/move.py
+++ Deplacement/Movement/move.py
@@ -44,14 +44,15 @@ class Move:
             Sen = [1,2,3]
         else:
             Sen = [4,5]
-
+        print(Sen)
+        
         self.SenOn = [0 for i in range(len(Sen))]
 
         while movAvg >= errorMax:
             Sen_count = 0
             #print("Values vaut : ", MCP3008.readadc(1) )
             #print("Encoder : ", axis.encoder.pos_estimate,"Goal/Target : ", goal, "movAvg : ", movAvg )
-            for i in range(len(Sen)):
+            for i in range(Sen):
                 if MCP3008.readadc(i) > 800 :
                     self.OBS = True
                     self.SenOn[i] = 1
