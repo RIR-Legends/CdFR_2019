6d7c44734ac7d85ee8f54e9d5d0ee821da9b9fd3
Lelyondepolytech <48736812+Lelyondepolytech@users.noreply.github.com>
2019-05-28 18:13:58 +0200

    Dev

--- Deplacement/Treatment.py
+++ Deplacement/Treatment.py
@@ -19,9 +19,7 @@ class Treatment:
     def step(self, p):
         from math import sqrt
 
-        # print("Coordonnées Points: X = ", int(p[0]), " Y = ", int(p[1]), "Theta =", int(p[2]))
-
-        print("P =", p)
+        print("Coordonnées Points: X = %f , Y = %f , Theta = %f " % (p[0], p[1], p[2]))
 
         self.deltaX = p[0] - self.X_abs
         self.deltaY = p[1] - self.Y_abs
