9c5210673c5672fc0e86b62a7360fa62b4f021e6
Lelyondepolytech <48736812+Lelyondepolytech@users.noreply.github.com>
2019-05-28 23:58:59 +0200

    changement de chemin
    homologuation ?

--- utils/Registre.py
+++ utils/Registre.py
@@ -15,9 +15,9 @@ class Creation:
     def main(self):
         # coté jaune
         self.chemin.add_point('aDepart', 500, 100, 90)
-        self.chemin.add_point('bPoint1', 500, 1500, 90)
-        self.chemin.add_point('cPoint2', 500, 500, -90)
-        self.chemin.add_point('dPoint3', 500, 500, 180)
+        self.chemin.add_point('bPoint1', 500, 1000, 90)
+        self.chemin.add_point('cPoint2', 500, 250, -90)
+        # self.chemin.add_point('dPoint3', 500, 500, 180)
         # self.chemin.add_point('Point4', 200, 200, 180)
 
         print(self.chemin.dictionnaire)
