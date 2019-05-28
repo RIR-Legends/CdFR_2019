cab13f5105a7e9541220fa51660de7ba34be1ff5
Yannou <45776332+YannLocatelli@users.noreply.github.com>
2019-05-28 22:39:19 +0200

    Implementation Timer (différent)

--- Robot.py
+++ Robot.py
@@ -17,17 +17,18 @@ class Robot():
     def __init__(self):
         # Initialisation de la communication avec l'Arduino
         self.com = Communication() ### ATTENTION PEUT ETRE BLOQUANT
-        
+
         # Initialisation informations extérieur
         self.side = None
         self.tirettePulled = False
-        
+        self.timer = time.time()
+
         # Initialisation ODrive
         self.Oparam = OdriveParam()
         #self.Oparam.RAZ()
         self.Oparam.config()
         self.Oparam.calib_always()
-        
+
         # Intilialisation Trajectoire
         self.move = Movements(self.Oparam.odrv0)
         self.dbPoints = filedb.fileDB(db="points")
@@ -35,10 +36,10 @@ class Robot():
         self.thetaOrder = 0
         self.distanceOrder = 0
         self.currentPos = Point(0,0,0)
-        
+
         # Initialisation Lidar et SLAM
         self.lidar = RPLidar() ### ATTENTION PEUT ETRE BLOQUANT
-        
+
     def checkSide(self):
         ''' A refaire selon branchement sur Rasp '''
         #while self.com.OrangeSide == None:
@@ -47,23 +48,27 @@ class Robot():
         #if OrangeSide:
         #    self.side = "Orange"
         #return self.side
+
+    def checkTimer(self):
+        Now = time.time() - DepartTime
         
+
     def waitingTrigger(self):
         ''' A refaire selon branchement sur Rasp '''
         #while self.com.Tirette:
         #    self.com.checkAndRead()
-            
+        self.timer = time.time()
+
     def stopAll(self):
         self.move.stop()
         self.com.send(Communication.MSG["Arret"])
-        
+
     def getOrder(self,departPoint,arrivalPoint):
         res = self.treatment(departPoint,arrivalPoint)
         self.thetaOrder = res[1]
         self.distanceOrder = res[0]
-    
+
     def getDataDB(self,name):
         ''' Va chercher et retourne le point associé au name dans la base de donnée '''
         data = literal_eval(self.dbPoints.get(name))
         return Point(data[0],data[1],data[2]) # Pour l'instant seuls les Points sont considérés dans la base de données
-        
--- utils/timer.py
+++ utils/timer.py
@@ -4,29 +4,26 @@
 from threading import Thread
 import time
 
-class NinetySec(Thread):
-    def __init__(self):
-        Thread.__init__(self)
 
-    def run(self):
-        DepartTime = time.time()
-        time.sleep(85)
+def RIR_timer(Motor, Lidar, Communication):
+    DepartTime = time.time()
+    time.sleep(5)
+    Now = time.time() - DepartTime
+    while Now < 10:
+        time.sleep(.1)
         Now = time.time() - DepartTime
-        while Now < 90:
-            time.sleep(.1)
-            Now = time.time() - DepartTime
-            #print(str(Now)+"\n")
-        #STOP ALL HERE
+        #print(str(Now)+"\n")
+    #STOP ALL HERE
 
 def main():
-    # Création des threads
-    thread = NinetySec()
-    
+    # Crï¿½ation des threads
+    launch_timer = threading.Thread(target=RIR_timer, args=(Odrive, RPLidar, Com))
+
     # Lancement des threads
     thread.start()
-    
+
     # Attend que les threads se terminent
-    thread.join()
-    
+    ##thread.join()
+
 if __name__ == '__main__':
     main()
