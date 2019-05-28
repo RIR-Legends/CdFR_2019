d3de6372e1bccc21e9ca6ed6e78e146e0dc7a0db
Thibault ROBERT <tbt.rbt@gmail.com>
2019-05-29 01:48:23 +0200

    Update calib_saved.py

--- Deplacement/Movement/calib_saved.py
+++ Deplacement/Movement/calib_saved.py
@@ -5,26 +5,27 @@ import odrive
 from odrive.enums import *  # a checker
 import time
 
+print("Recherche Odrive...")
 odrv0 = odrive.find_any()
-
+print("Lancement Calib & Sauvegarde config")
 # 40Amp max dans le moteur (gros couple et sécurité pour pas fumer le moteur)
-self.odrv0.axis0.motor.config.current_lim = 10
-self.odrv0.axis1.motor.config.current_lim = 10
+odrv0.axis0.motor.config.current_lim = 10
+odrv0.axis1.motor.config.current_lim = 10
 
 # vmax en tick/s les encodeurs font 8192 tick/tours
 # controller.*.vel_limite prend le pas sur trap_traj.*.vel_limt
-self.odrv0.axis0.controller.config.vel_limit = 50000
-self.odrv0.axis1.controller.config.vel_limit = 50000
+odrv0.axis0.controller.config.vel_limit = 50000
+odrv0.axis1.controller.config.vel_limit = 50000
 
 # trap_traj parametrage des valeurs limit du comportement dynamique
-self.odrv0.axis1.trap_traj.config.vel_limit = 40000
-self.odrv0.axis0.trap_traj.config.vel_limit = 40000
+odrv0.axis1.trap_traj.config.vel_limit = 40000
+odrv0.axis0.trap_traj.config.vel_limit = 40000
 
-self.odrv0.axis0.trap_traj.config.accel_limit = 10000
-self.odrv0.axis1.trap_traj.config.accel_limit = 10000
+odrv0.axis0.trap_traj.config.accel_limit = 10000
+odrv0.axis1.trap_traj.config.accel_limit = 10000
 
-self.odrv0.axis0.trap_traj.config.decel_limit = 10000
-self.odrv0.axis1.trap_traj.config.decel_limit = 10000
+odrv0.axis0.trap_traj.config.decel_limit = 10000
+odrv0.axis1.trap_traj.config.decel_limit = 10000
 
 odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 odrv0.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
