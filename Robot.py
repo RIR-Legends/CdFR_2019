0a6cfc10d989c730f95d9072b3d63b3eac15ccb0
Yannou <45776332+YannLocatelli@users.noreply.github.com>
2019-05-30 14:29:33 +0200

    Fix bugs
    Objet mal indiqué

--- Robot.py
+++ Robot.py
@@ -22,16 +22,16 @@ from utils.timer import RIR_timer
 class Robot():
     def __init__(self, lancer_exp = True, MatCode = False):
         # Initialisation variables
-        self.db = filedb.fileDB(db = "../Points")
+        self.db = filedb.fileDB(db = "Points")
         self.__lastpoint = Point.get_db_point("Point0", self.db)
         self.__side = Switch.cote()
         self.__com = Communication('/dev/ttyACM0')
         self.__Oparam = Param()
-        self.__move = Move(self.Oparam.odrv0)
+        self.__move = Move(self.__Oparam.odrv0)
         self.__MatCode = MatCode
         self.__traj = Trajectoire(param = self.__Oparam, move = self.__move, point = self.__lastpoint, Solo = self.__MatCode)
         self.__lidar = RPLidar('/dev/ttyUSB0') #self.__lidar = Lidar('/dev/ttyUSB0')
-        self.__timer = RIR_timer(self.com, (self.param,self.move), self.lidar, launch_exp) # Test: placé avant __init_physical
+        self.__timer = RIR_timer(self.__com, (self.__Oparam,self.__move), self.__lidar, launch_exp) # Test: placé avant __init_physical
                 
         self.__init_physical()
         self.set_ready()
@@ -57,14 +57,14 @@ class Robot():
         
     def action(self, action_name, dist_deploiement = 100):
         if action_name == "Transport" or action_name == "Palet_Floor_In" or action_name =="Palet_Floor_Out":
-            self.com.waitEndMove(Communication.MSG[action_name])
+            self.__com.waitEndMove(Communication.MSG[action_name])
         elif action_name == "Arret":
-            self.com.send(Communication.MSG[action_name])
+            self.__com.send(Communication.MSG[action_name])
         elif action_name == "Palet_Wall_In" or action_name == "Palet_Wall_Out":
-            self.com.send(Communication.MSG[action_name])
+            self.__com.send(Communication.MSG[action_name])
             
-            while not self.com.Avancer:
-                self.com.read()
+            while not self.__com.Avancer:
+                self.__com.read()
             temp_point = self.__lastpoint
             if action_name == "Palet_Wall_In":
                 temp_point.x += dist_deploiement
@@ -72,11 +72,11 @@ class Robot():
                 temp_point.x -= dist_deploiement
             self.__traj.process(temp_point)
             
-            self.com.send(Communication.MSG["Action_Finished"])
-            while not self.com.Reculer:
-                self.com.read()
+            self.__com.send(Communication.MSG["Action_Finished"])
+            while not self.__com.Reculer:
+                self.__com.read()
             self.__traj.process(self.__lastpoint)
             
             self.com.send(Communication.MSG["Action_Finished"])
-            while not self.com.readyNext:
-                self.com.read()
+            while not self.__com.readyNext:
+                self.__com.read()
