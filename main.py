65b60597e5142984e600f500b735fc1d078141f3
Yannou <45776332+YannLocatelli@users.noreply.github.com>
2019-05-31 13:10:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main(lancer_exp = True, MatCode = False, db = "Points"):
     robot.move_to("Point1")        ##Exemple pour se déplacer à un point
     #robot.action("Palet_Floor_In")    ##Exemple pour effectuer une action avec l'Arduino
     robot.move_to("Point2")
-    robot.move_to("Point3", revert=True)
+    robot.move_to("Point3")
     robot.move_to("Point4")
     robot.action("Palet_Floor_Out")
     #robot.move_to("Point4")
