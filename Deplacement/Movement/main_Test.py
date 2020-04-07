351d38a4141f2c7cae5e92c3718b77bd49850f3e
Thibault ROBERT <tbt.rbt@gmail.com>
2020-04-07 11:18:12 +0200

    Update main_Test.py

--- Deplacement/Movement/main_Test.py
+++ Deplacement/Movement/main_Test.py
@@ -98,11 +98,11 @@ def run_test(odrv0) :
 
 """ Paramétrage et Calibration """
 param = p.Param()
-param.config()
-param.calib()
+
 param.raz()# Lance fonction remise à zero des moteurs
-#param.config()  #Lance la configuration du odrive (vitesse max / acc max / decc max / courrant max ...)
-#param.calib()
+sleep(5)
+param.config()  #Lance la configuration du odrive (vitesse max / acc max / decc max / courrant max ...)
+param.calib()
 """ ------------------------------- """
 
 """ Choix de lancement des demos : """
